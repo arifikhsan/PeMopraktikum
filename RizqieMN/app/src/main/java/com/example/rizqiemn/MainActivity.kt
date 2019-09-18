1eb483d5cc9fcdc5bcd7ca3d5851d6a1cd08fd80
arifikhsan <arif.ikhsanudin.developer@gmail.com>
2019-09-18 17:02:56 +0700

    aaaaa

--- RizqieMN/app/src/main/java/com/example/rizqiemn/MainActivity.kt
+++ RizqieMN/app/src/main/java/com/example/rizqiemn/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.rizqiemn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.textSize = 64F
+        textView.text = "\uD83D\uDE28\uD83D\uDE28\uD83D\uDE28\uD83D\uDE28\uD83D\uDE28\uD83D\uDE28\uD83D\uDE28\uD83D\uDE28"
     }
 }
